77368b321d228f3231e89c67547a26697c4b06a0
Tobias Habermann <wegwerf2@numericserver.de>
2018-02-05 14:56:34 +0100

    Insert gefixt

--- FederalDatabaseAPI/src/Data/SelectHelper.java
+++ FederalDatabaseAPI/src/Data/SelectHelper.java
@@ -37,11 +37,13 @@ public final class SelectHelper {
                 select_sql += ", ";
         }
         select_sql += " from " + table.toLowerCase();
+        return null;
     }
     
     private static PreparedStatement getPrepStatement(Connection connection)
     {
-        for(PreparedStatement prepStat : preparedStatments)
-            if(prepStat.getConnection() == i)
+        //for(PreparedStatement prepStat : preparedStatments)
+         //   if(prepStat.getConnection() == i)
+        return null;
     }
 }
