31e076e7dccff68512932172ebff141aa32af1ad
Tobias Habermann <wegwerf2@numericserver.de>
2018-02-26 11:29:25 +0100

    kleiner fix wegen testdaten

--- FederalDatabaseAPI/src/FederalDB/FedConnection.java
+++ FederalDatabaseAPI/src/FederalDB/FedConnection.java
@@ -16,7 +16,7 @@ public class FedConnection implements FedConnectionInterface{
     private Connection conn[] = new Connection[3];
     public final MetaDataManager metaDataManger;
 
-    public FedConnection(Connection[] conns) throws SQLException, FedException{
+    public FedConnection(Connection[] conns) {
         this.conn = conns;
         metaDataManger = new MetaDataManager(conn[0]);
         /*
